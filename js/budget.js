7a85eb51e4bc26b7c6a2e598efb6594c49403f13
unknown <rotsynomena9@gmail.com>
2026-05-10 18:51:20 +0300

    adding function diplayTransaction

--- js/budget.js
+++ js/budget.js
@@ -1,24 +1,28 @@
 const categoriesSelect = document.getElementById("trans-category")
 const subCategoriesSelect = document.getElementById("trans-subcategory")
-const categories = JSON.parse(localStorage.getItem("pfm_categories"))
 const expensebtn = document.getElementById("type-expense")
 const incomebtn = document.getElementById("type-income")
+const saveTransactionbtn = document.getElementById("btn-save-transaction")
+const type = document.querySelector(".type-btn.active")
+const label = document.getElementById("trans-label")
+const displaytransact = document.getElementById("display-transaction")
 let transactions = [];
 
 function loadTransaction() {
-    if(localStorage.getItem("pfm_transaction")) {
-        transactions = JSON.parse(localStorage.getItem("pfm_transaction"));
+    if(localStorage.getItem("pfm_transactions")) {
+        transactions = JSON.parse(localStorage.getItem("pfm_transactions"));
     }
 }
 
 function saveTransaction() {
-    localStorage.setItem("pfm_transaction", JSON.stringify(transactions));
+    localStorage.setItem("pfm_transactions", JSON.stringify(transactions));
 }
 
 function selectCategories () {
     categoriesSelect.innerHTML = '<option value="">Select a category</option>';
     if (localStorage.getItem("pfm_categories")) {
-        categories.forEach(element => {
+        const categ = JSON.parse(localStorage.getItem("pfm_categories"))
+        categ.forEach(element => {
             const choice = document.createElement("option")
             choice.value = element.id;
             choice.textContent = element.name;
@@ -27,13 +31,15 @@ function selectCategories () {
     }
 }
 
-function selectSubcategories() {
+categoriesSelect.addEventListener("change", function() {
     subCategoriesSelect.innerHTML = '<option value="">Subcategories</option>';
     const opt = document.createElement("option");
+
     if (!this.value) {
         return;
     }
     if (localStorage.getItem("pfm_categories")) {
+        const categories = JSON.parse(localStorage.getItem("pfm_categories"))
         const search = categories.find(categ => categ.id == this.value)
         if (search && search.subCategories) {
             search.subCategories.forEach(subCateg => {
@@ -43,8 +49,7 @@ function selectSubcategories() {
             });
         }
     }
-}
-CategoriesSelect.addEventListener("change", selectCategories());
+});
 
 expensebtn.addEventListener("click", () => {
     expensebtn.classList.add("active");
@@ -55,6 +60,52 @@ incomebtn.addEventListener("click", () => {
     incomebtn.classList.add("active");
     expensebtn.classList.remove("active");
 })
+
+saveTransactionbtn.addEventListener("click", () => {
+    const isExpense = document.getElementById("type-expense").classList.contains("active");
+    const type = isExpense ? "expense" : "income";
+    const amount = parseFloat(document.getElementById("trans-amount").value);
+    const categId = categoriesSelect.value;
+    const subCateg = subCategoriesSelect.value;
+    const date = document.getElementById("trans-date").value;
+
+    if (!label.value.trim || !amount || !categId || !date) {
+        alert("Please fill in all fields!");
+        return;
+    }
+    const newTransaction = {
+        id: Date.now(),
+        type: type,
+        label: label,
+        amount: amount,
+        categId: parseInt(categId),
+        subCateg: subCateg,
+        date: date
+    };
+    transactions.push(newTransaction);
+    saveTransaction();
+    
+    label.value = "";
+    document.getElementById("trans-amount").value = "";
+    subCategoriesSelect.innerHTML = '<option value="">Subcategory</option>';
+})
+
+function displayTransaction() {
+    displaytransact.innerHTML = "";
+
+    transactions.forEach(trans => {
+        const div = document.createElement("div");
+        div.className = `transaction-item ${trans.type}`;
+        div.innerHTML = `
+            <span class = "trans-date">${trans.date}</span>
+            <div class = "trans-details>
+                <strong>${trans.label}</strong><br>
+                <small>${trans.subCateg || ''}</small>
+            </div>
+        `
+    })
+}
 document.addEventListener("DOMContentLoaded", () => {
-    loadTransactions();
+    loadTransaction();
+    selectCategories();
 });
