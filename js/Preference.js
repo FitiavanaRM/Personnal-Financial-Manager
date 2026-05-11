1bb9c4af4209ebd34314a2ba5ae348c6040d9419
unknown <rotsynomena9@gmail.com>
2026-05-11 10:05:32 +0300

    Adding HTML code for Upcoming

--- js/Preference.js
+++ js/Preference.js
@@ -1,13 +1,14 @@
 
-function getPreference() {
+/*function getPreference() {
     let preferences = {};
     const data = localStorage.getItem("pfm_preferences")
     preferences = data ? JSON.parse(data) : {
         devise : "Ar",
         date : "jj/mm/aaaa"
     };
+    return preferences;
 }
 
 function savePreference() {
-   
-}
+    
+}*/
