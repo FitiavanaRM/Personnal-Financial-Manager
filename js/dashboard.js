d7482601c87cfacbd81dea9a2f2e76c907b5e638
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-05-12 15:18:06 +0300

    Adding function updateSummary

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,6 @@
+const dashBalance = document.getElementById("dash-balance")
+const dashIncome = document.getElementById("dash-income")
+const dashExpense = document.getElementById("dash-expense")
 
 function loadDashboard() {
   
@@ -14,4 +17,26 @@ function loadDashboard() {
   if(localStorage.getItem("pfm_upcoming")) {
     upcoming = JSON.parse(localStorage.getItem("pfm_upcoming"));
   }
+}
+
+function updateSummary(transactions) {
+  let totalIncome = 0;
+  let totalExpense = 0;
+
+  transactions.forEach(element => {
+    if(element.type === "income") {
+      totalIncome += element.amount;
+    }
+    else {
+      totalExpense += element.amount;
+    }
+  }); 
+  
+  const balance = totalIncome - totalExpense;
+  dashBalance.textContent = balance.toLocaleString("fr-MG") + "Ar";
+  dashIncome.textContent = totalIncome.toLocaleString("fr-MG") + "Ar";
+  dashExpense.textContent = totalExpense.toLocaleString("fr-MG") + "Ar";
+
+  dashBalance.style.color = balance >= 0 ? "#22d1bc" : "#ef4444";
+
 }
