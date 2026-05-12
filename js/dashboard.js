3c60b5ae70f6a7965e33354bafcf3a221cce7ca6
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-05-12 15:06:45 +0300

    Adding function loadDashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,17 @@
 
+function loadDashboard() {
+  
+  let transactions = [];
+  let goals = [];
+  let upcoming = [];
+
+  if (localStorage.getItem("pfm_transactions")) {
+    transactions = JSON.parse(localStorage.getItem("pfm_transactions"));
+  }
+  if(localStorage.getItem("pfm_goals")) {
+    goals = JSON.parse(localStorage.getItem("pfm_goals"));
+  } 
+  if(localStorage.getItem("pfm_upcoming")) {
+    upcoming = JSON.parse(localStorage.getItem("pfm_upcoming"));
+  }
+}
